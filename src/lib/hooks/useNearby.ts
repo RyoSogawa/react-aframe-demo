1e5d3acce6753973feb9db7421b7b6957ef36794
RyoSogawa <koashimitekara.1122@gmail.com>
2022-01-26 23:59:44 +0900

    set swr data to immutable

--- src/lib/hooks/useNearby.ts
+++ src/lib/hooks/useNearby.ts
@@ -1,10 +1,10 @@
-import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 import { fetcher } from '../fetcher'
 import type { Fetcher } from 'swr'
 import type { PlacesNearbyResponseData } from '@googlemaps/google-maps-services-js'
 
 export const useNearby = (shouldSkip: boolean, params: NearbyParams) => {
-  return useSWR(
+  return useSWRImmutable(
     !shouldSkip && '/api/google-map/nearby?' + new URLSearchParams(params),
     fetcher as Fetcher<PlacesNearbyResponseData>
   )
