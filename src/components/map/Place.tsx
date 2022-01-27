d9a0c9264cd3dac15260ea9b235de2b39cc05870
RyoSogawa <koashimitekara.1122@gmail.com>
2022-01-27 11:19:10 +0900

    add loading object for giphy api

--- src/components/map/Place.tsx
+++ src/components/map/Place.tsx
@@ -4,12 +4,16 @@ import AImage from '../aframe/AImage'
 import ACone from '../aframe/ACone'
 import { useSearchGiphy } from '../../lib/hooks/useSearchGiphy'
 import { convertAddressTypeToURI } from '../../lib/utils'
+import APlane from '../aframe/APlane'
+import AText from '../aframe/AText'
 
 export type PlaceProps = Partial<PlaceData>
 
 const Place = (props: PlaceProps): JSX.Element => {
   const { place_id, geometry, icon_background_color, types } = props
-  const { data } = useSearchGiphy({ keyword: convertAddressTypeToURI(types) })
+  const { data, isValidating } = useSearchGiphy({
+    keyword: convertAddressTypeToURI(types),
+  })
 
   return (
     <>
@@ -20,6 +24,26 @@ const Place = (props: PlaceProps): JSX.Element => {
         width={0.5}
         height={2}
       />
+      {isValidating && (
+        <>
+          <APlane
+            gps-Entity-Place={`latitude: ${geometry?.location.lat}; longitude: ${geometry?.location.lng};`}
+            position={'0 4 2'}
+            color={'white'}
+            width={8}
+            height={6}
+          />
+          <AText
+            value={'loading...'}
+            gps-Entity-Place={`latitude: ${geometry?.location.lat}; longitude: ${geometry?.location.lng};`}
+            position={'0 4 0'}
+            color={'black'}
+            width={18}
+            align="center"
+            z-Offset={1}
+          />
+        </>
+      )}
       {data && (
         <AImage
           src={data.image.mp4}
